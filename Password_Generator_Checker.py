5bc54bd2758f1806779115e725abf8e17d5bb633
AdBnj <143836083+AdBnj@users.noreply.github.com>
2023-09-13 16:32:54 +0100

    Password_Generator_Checker.py

--- Password_Generator_Checker.py
+++ Password_Generator_Checker.py
@@ -1,169 +1,94 @@
-
-# Import necessary libraries
-from tkinter import messagebox
-import tkinter as tk
-from tkinter import *
-import re
 import random
+import re
+from tkinter import Tk, Button, Text, Label, Entry, Frame, messagebox, WORD, FALSE
 
-# Define various character sets to be used in password generation and checking
-characters = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!$%^&*()-_=+"
-symb = "!$%^&*()-_=+"
-badsymb = "`~@#^[]\\{}|;':,./<>?£"
-badcharacters = "[£|{|[|}|]|:|;|@|'|~|#|<|,|>|.|?|/|`|¬|\\|||]"
-Uqwerty = ('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMN')
+CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!$%^&*()-_=+"
 
-# Function to generate a random password and check its strength
-def Generate():
-    
-    # Nested function to generate a random password and calculate its strength score
-    def GeneratePassword():
-        import random
-        from random import randrange
-    
-        # Generate a random password length between 8 and 12 characters
-        passlength = randrange(8, 12)
+class PasswordApp:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Password Generator and Checker")
+        self.frame = Frame(self.root)
+
+        self.setup_menu()
+
+    def setup_menu(self):
+        Label(self.root, text="Please choose one of the options:").pack()
         
-        # Loop to continuously generate passwords until a strong one is created
+        Button(self.frame, text='Password Generator', command=self.generate_password_gui).pack(side='left')
+        Button(self.frame, text='Password Checker', command=self.check_password_gui).pack(side='left')
+        Button(self.frame, text='Exit the Program', command=self.exit_program).pack(side='left')
 
+        self.frame.pack()
+
+    def generate_password_gui(self):
+        self.window = Tk()
+        self.window.title("Password Generator")
+        self.window.geometry("250x150")
+        self.window.resizable(width=FALSE, height=FALSE)
+        
+        Button(self.window, text="Generate", command=self.generate_password).pack()
+        
+        self.results = Text(self.window, width=35, height=5, wrap=WORD)
+        self.results.pack()
+
+    def check_password_gui(self):
+        self.window = Tk()
+        self.window.title("Password Checker")
+        self.window.geometry("250x150")
+        self.window.resizable(width=FALSE, height=FALSE)
+        
+        Label(self.window, text="Password:").pack()
+        
+        self.password_guess = Entry(self.window)
+        self.password_guess.pack()
+        
+        Button(self.window, text="Submit", command=self.check_password).pack()
+        
+        self.result = Text(self.window, width=35, height=5, wrap=WORD)
+        self.result.pack()
+
+    def generate_password(self):
+        pass_length = random.randint(8, 12)
         while True:
-            passw =  "".join(random.sample(characters,passlength))
-            score = len(passw)
-            if len(passw) < 8:
-                window2.destroy()
-            if len(passw) > 24:
-                window2.destroy()
-            if re.search("[A-Z]", passw):
-                score += 5
-            if re.search("[a-z]", passw):
-                score += 5
-            if re.search("[0-9]", passw):
-                score += 5
-            if re.search("[|!|$|%|^|&|*|(|)|-|_|=|+|]", passw):
-                score += 5
-            if re.search("[a-z]", passw) and re.search("[A-Z]", passw) and re.search("[0-9]", passw)and re.search("[|!|$|%|^|&|*|(|)|-|_|=|+|]", passw):
-                score += 10
-            if passw.isdigit():
-                score+=-5
-            if passw.isalpha():
-                score+=-5
+            password = "".join(random.sample(CHARACTERS, pass_length))
+            score = self.calculate_score(password)
             if score > 20:
-                message = "Passowrd:  " + passw
-
-            # Various conditions to check the strength of the generated password and calculate a score 
-            # (Checking the length, presence of uppercase letters, lowercase letters, digits, and special characters)
-            # ... (Your existing conditions and score calculations here) ...
-            
-            # Display the generated password and its score in the GUI
-            results.delete(0.0, END)
-            results.insert(0.0, message)
-            if score > 20:
+                message = f"Password: {password}"
+                self.results.delete(0.0, 'end')
+                self.results.insert(0.0, message)
                 break
 
-# GUI setup for the password generator
-    window2 = Tk()
-    window2.resizable(width=FALSE, height=FALSE)
-    window2.title("Password Generator")
-    window2.geometry("250x150")
-#Generate Button
-    bttnGenerate = Button(window2, text="Generate", command=GeneratePassword)
-    bttnGenerate.pack()
-#Creating Text Grid
-    results = Text(window2, width = 35, height = 5, wrap = WORD)
-    results.pack()
-#Main Starter
-    window2.mainloop()
-
- # Function to check the strength of a user-input password
-def Check():
-    
-    # Nested function to check the strength of a user-input password and calculate its score
-    def checkPassword():
-        Upassw = password_guess.get()
-        Uscore = len(Upassw)
-        import re
-        if len(Upassw) < 8:
-            dialog_title = 'Returning to Menu.'
-            dialog_text = 'Error 1: Password too small.'
-            answer = messagebox.showinfo(dialog_title, dialog_text)
-            window.destroy()
-        if len(Upassw) > 24:
-            Uscore +=-1000
-            dialog_title = 'Returning to Menu.'
-            dialog_text = 'Error 2: Password too large.'
-            answer = messagebox.showinfo(dialog_title, dialog_text)
-            window.destroy()
-        if re.search("[A-Z]", Upassw):
-            Uscore += 5
-        if re.search("[a-z]", Upassw):
-            Uscore += 5
-        if re.search("[0-9]", Upassw):
-            Uscore += 5
-        if re.search("[|!|$|%|^|&|*|(|)|-|_|=|+|]", Upassw):
-            Uscore += 5
-        if re.search("[a-z]", Upassw) and re.search("[A-Z]", Upassw) and re.search("[0-9]", Upassw)and re.search("[|!|$|%|^|&|*|(|)|-|_|=|+|]", Upassw):
-            Uscore += 10
-        if Upassw.isdigit():
-            Uscore+=-5
-        if Upassw.isalpha():
-            Uscore+=-5
-        if Uscore > 20:
-            write = "Your Password is Strong! It scored",Uscore,"points!"
-        if Uscore > 0 and Uscore < 20:
-            write = "Your Password is Weak, it onlyscored",Uscore,"points!"
-        if Uscore <= 0:
-            write = "Your Password is Weak, it onlyscored",Uscore,"points!"
-        result.insert(tk.END, write)
-        result.see(tk.END)
+    def check_password(self):
+        user_input = self.password_guess.get()
+        message = self.check_password_strength(user_input)
         
-#Gui set up for the Password Checker
-    window = Tk()
-    window.resizable(width=FALSE, height=FALSE)
-    window.title("Password Checker")
-    window.geometry("250x150")
+        self.result.insert('end', message)
+        self.result.see('end')
 
-#Creating the password entry box
-    password_text = Label(window, text="Password:")
-    password_guess = Entry(window)
+    def check_password_strength(self, user_input):
+        score = self.calculate_score(user_input)
+        if score > 20:
+            message = f"Your Password is Strong! It scored {score} points!"
+        else:
+            message = f"Your Password is Weak, it only scored {score} points!"
+        return message
 
-#Insert Password
-    Submit = Button(window, text="Submit", command=checkPassword)
-    password_text.pack()
-    password_guess.pack()
-    Submit.pack()
+    def calculate_score(self, password):
+        score = len(password)
+        if re.search("[A-Z]", password): score += 5
+        if re.search("[a-z]", password): score += 5
+        if re.search("[0-9]", password): score += 5
+        if re.search("[!$%^&*()-_=+]", password): score += 5
+        if all(re.search(pattern, password) for pattern in ("[A-Z]", "[a-z]", "[0-9]", "[!$%^&*()-_=+]")):
+            score += 10
+        if password.isdigit() or password.isalpha(): score -= 5
+        return score
 
-#Creating Text Grid
-    result = Text(window, width = 35, height = 5, wrap = WORD)
-    result.pack()
-    
-#Main Starter
-    window.mainloop()# Class to create the main menu of the application
-class MenuC():
-    root = Tk()
-    frame = Frame(root)
-    
-    # Set up the main window with a title and buttons to access the password generator and checker
-    root.title("Password Generator and Checker")
-    
-    # Define buttons to access the password generator, password checker, and to exit the program
-    bgen = Button(frame, text='Password Generator', command=Generate)
-    bcheck = Button(frame, text='Password Checker', command=Check)
-    
-    # Define an exit button with a confirmation dialog
-    def Exit():
-        dialog_title = 'EXIT'
-        dialog_text = 'Are you sure?'
-        answer = messagebox.askquestion(dialog_title, dialog_text)
-        if answer == 'yes':
-            quit()
-    bexit = Button(frame, text= 'Exit the Program', command=Exit)
-    
-    # Arrange buttons in the frame
-    bgen.pack(side=LEFT)
-    bcheck.pack(side=LEFT)
-    bexit.pack(side=LEFT)
-    
-    # Add a label and pack the frame into the root window
-    l1 = Label(root, text="Please choose one of the options:")
-    l1.pack()
-    frame.pack()
+    def exit_program(self):
+        if messagebox.askquestion('EXIT', 'Are you sure?') == 'yes':
+            self.root.destroy()
+
+if __name__ == '__main__':
+    app = PasswordApp()
+    app.root.mainloop()
